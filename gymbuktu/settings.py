e9627086b06e27e58d5f8b49d38e0b2600e45a10
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2025-01-26 23:11:49 +0000

    Secret key added and linked in settings

--- gymbuktu/settings.py
+++ gymbuktu/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gtr)ba!srk)^we*fk00dueuve(#y0mwo)q&jr*g33mz9qc$&=&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = 'django-insecure-gtr)ba!srk)^we*fk00dueuve(#y0mwo)q&jr*g33mz9qc$&=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
