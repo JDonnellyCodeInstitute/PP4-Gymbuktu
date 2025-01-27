db37bb61f66c52135841dabaec07f5d27725582c
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2025-01-27 10:38:11 +0000

    Facility model created and added to admin page

--- facilities/admin.py
+++ facilities/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Facility
+
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'max_capacity')
--- facilities/models.py
+++ facilities/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Facility(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=20)
+    max_capacity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
