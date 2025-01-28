3741060e5042f4af2bb02c1b4dd32de5dd799caf
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2025-01-28 22:09:54 +0000

    Updated sign up view to log user in after sign up and direct them to the profile page. Profile view added

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from classes.models import Booking
 
 def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login') # Redirecting to login page once registered
+            user = form.save()
+            login(request, user)
+            return redirect('profile')  # Redirecting to profile after signup
 
     else:
         form = SignUpForm()
-    return render(request, 'accounts/signup.html', {'form': form}) # Returns sign up template
+    return render(request, 'accounts/signup.html', {'form': form})  # Returns sign-up template
+
+@login_required
+def profile(request):
+    current_bookings = Booking.objects.filter(user=request.user, status="upcoming")
+    past_bookings = Booking.objects.filter(user=request.user, status="completed")
+
+    return render(
+        request,
+        'accounts/profile.html',
+        {
+            'current_bookings': current_bookings,
+            'past_bookings': past_bookings,
+        }
+    )
