ed9a9ca5d39d26360a4a76b6049f1329e3c20357
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2025-01-28 19:44:49 +0000

    Fixed login and logout URLs

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
