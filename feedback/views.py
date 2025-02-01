0521723fac26a5a0ef78b022fb7fd4905e5c0bc3
JoeyDPD <joe.donnelly.bmc@gmail.com>
2025-02-01 13:46:09 +0000

    Feedback view created including functionality to allow users to see their previous feedback submissions

--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Feedback
+from .forms import FeedbackForm
 
-# Create your views here.
-def feedback_test(request):
-    return HttpResponse("Hello, World!")
+@login_required
+def feedback_view(request):
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.user = request.user
+            feedback.save()
+            return redirect("feedback_received")
+    else:
+        form = FeedbackForm()
+
+    feedback_entries = Feedback.objects.filter(user=request.user).order_by("-timestamp")
+
+    return render(request, "feedback/feedback_form.html", {"form": form, "feedback_entries": feedback_entries})
