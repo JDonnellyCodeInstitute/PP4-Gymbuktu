0710db7d786cf327c307b917b3a69a0260383bb9
JoeyDPD <joe.donnelly.bmc@gmail.com>
2025-02-03 12:54:45 +0000

    **BUG FIX** Individual users should only be able to book themselves into the same class once. This was implemented by adding an if statement to a variable existing_booking in the book class view

--- classes/views.py
+++ classes/views.py
@@ -14,8 +14,20 @@ def class_detail(request, class_id):
 def book_class(request, class_id):
     gym_class = get_object_or_404(Class, id=class_id)
     
+    # Check if user has already booked class
+    existing_booking = Booking.objects.filter(user=request.user, gym_class=gym_class, class_status=0).exists()
+    
+    if existing_booking:
+        return render(request, "classes/booking_confirmation.html", {
+            "gym_class": gym_class,
+            "error": "You have already booked this class."
+        })
+    # Confirm there is a space available in the class
     if gym_class.available_slots() <= 0:
-        return render(request, "classes/booking_confirmation.html", {"gym_class": gym_class, "error": "No available slots."})
+        return render(request, "classes/booking_confirmation.html", {
+            "gym_class": gym_class,
+            "error": "No available slots."
+        })
     
     if request.method == "POST":
         Booking.objects.create(user=request.user, gym_class=gym_class)
@@ -30,10 +42,10 @@ def booking_confirmation(request, class_id):
 @login_required
 def cancel_booking(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id, user=request.user)
-    
+
     if request.method == "POST":
         booking.class_status = 1
         booking.save()
         return redirect("class_list")
-    
+
     return render(request, "classes/cancel_booking.html", {"booking": booking})
