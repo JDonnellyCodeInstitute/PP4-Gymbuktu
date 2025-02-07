f5fbe6fe3476f89be94085562a5eeb38f9c29ad9
JoeyDPD <joe.donnelly.bmc@gmail.com>
2025-02-07 11:23:13 +0000

    Model added to accounts app to give each user a unique token which will be used to authenticate their account

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+import uuid
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class EmailVerification(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.UUIDField(default=uuid.uuid4, unique=True)
+    is_verified = models.BooleanField(default=False)
