17cdbf8f28d0df8a89f0f45b29b838fa06351d7e
JoeyDPD <joe.donnelly.bmc@gmail.com>
2025-02-13 14:13:34 +0000

    Accounts URLs updated to include pathway to django's built-in password reset functionality. Link added to homepage to password_reset template - creating it next

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,14 +4,38 @@ from . import views
 from .forms import CustomLoginForm
 
 urlpatterns = [
+    # User Authentication
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(
         template_name='accounts/login.html',
         authentication_form=CustomLoginForm
     ), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(
+        template_name='accounts/logout.html'
+    ), name='logout'),
+
+    # Profile & Email Verification
     path('profile/', views.profile, name='profile'),
     path('check-email/', views.check_email, name='check_email'),
     path('verify/<uuid:token>/', views.verify_email, name='verify_email'),
     path('already-verified/', views.already_verified, name='already_verified'),
+
+    # Password Reset
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset.html'
+    ), name='password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+          template_name='accounts/password_reset_confirm.html'
+         ), name='password_reset_confirm'),
+
+    path('password-reset-complete/', 
+         auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_complete.html'
+         ), name='password_reset_complete'),
 ]
