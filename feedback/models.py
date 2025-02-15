cf82304e86a933b61f49a78e68369abe72ef97d0
JoeyDPD <joe.donnelly.bmc@gmail.com>
2025-02-15 16:17:19 +0000

    PEP8 alignment feedback/models.py

--- feedback/models.py
+++ feedback/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Feedback(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Feedback from {self.user.username} at {self.timestamp.strftime('%Y-%m-%d %H:%M')}"
+        return f"Feedback from {self.user.username} at {
+            self.timestamp.strftime('%Y-%m-%d %H:%M')}"
